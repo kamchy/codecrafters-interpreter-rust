9b351350bb2413db8bfcd21a951b283a05289396
Kamila Chyla <230475+kamchy@users.noreply.github.com>
2024-08-29 14:11:37 +0200

    Return proper ExitCode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::process::ExitCode;
 mod lexer;
 mod token;
-fn main() {
+fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
+        return ExitCode::FAILURE;
     }
 
     let command = &args[1];
@@ -19,21 +20,26 @@ fn main() {
                 eprintln!("Failed to read file {}", filename);
                 String::new()
             });
-            tokenize(&file_contents);
+            tokenize(&file_contents)
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            return ExitCode::FAILURE;
         }
     }
 }
 
-fn tokenize(s: &str) {
+fn tokenize(s: &str) -> ExitCode {
     let lexer = lexer::Lexer::new(s);
+    let mut exit_code = ExitCode::SUCCESS;
     for token in lexer {
         match token {
-            token::Token::Unknown(c) => eprintln!("[line 1] Error: Unexpected character: {}", c),
+            token::Token::Unknown(c) => {
+                eprintln!("[line 1] Error: Unexpected character: {}", c);
+                exit_code = ExitCode::from(65);
+            }
             _ => println!("{}", token),
         }
     }
+    exit_code
 }
